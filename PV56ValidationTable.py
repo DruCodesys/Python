d8d8762134af262ed1f34e7b264367da82af913f
LS-KS <116895972+LS-KS@users.noreply.github.com>
2023-02-22 13:18:34 +0100

    Reihenfolge der Spalten geändert. Einstellparameter an den Kopf des Programms gezogen. Programm auskommentiert

--- PV56ValidationTable.py
+++ PV56ValidationTable.py
@@ -5,17 +5,25 @@ Datum: 21.02.2023
 --> Keine Kopf-Aktualisierung. Versionsverfolgung erfolgt über Git und GitHub.
 '''
 
-#imports
-import numpy as np
-from matplotlib import pyplot as plt
-import pandas as pd
-import tkinter as tk
-from tkinter import messagebox
 import os
+from tkinter import messagebox
+import pandas as pd
 
-#Dateipfad des USB-Sticks angeben. Für Entwicklungszwecke zunächst vom Root-Verzeichnis aus.
-FILE = "223061_fehlerhaft.csv"
-USB = "C:/Users/lschi/Documents/GitHub/Python/"
+#------PROGRAMMPARAMETER-----#
+#TODO: Die Variable 'USB' muss das Hauptverzeichnis des USB-Sticks sein.
+USB = "C:/USers/pink_/Documents/GitHub/Python/"
+#USB = "C:/Users/lschi/Documents/GitHub/Python/"
+
+#TODO: Bemessungsgrenze für Durchflusswert und Druck
+DRUCK = 3
+FLOW = 8
+
+#TODO: Name der Ergebnisdatei im .md-Format
+#TODO: Bitte Beachten: DIe Ergebnisdatei manuell löschen! Dieses Script hängt immer nur an die aktuelle Datei an.
+ERGEBNIS = "Ergebnis.md"
+
+#-----PROGRAMM-----#
+#For-Schleife wird für jede Datei im Verzeichnis 'USB' ausgeführt
 for root, dirs, files in os.walk(USB):
         for FILE in files:
             if FILE.endswith(".csv"):
@@ -24,21 +32,26 @@ for root, dirs, files in os.walk(USB):
 
                 #Header sind Zeilen 0, 8, 17 --> Unregelmäßig
                 #Metadaten sind in den Zeilen 1, 9, 18 --> Unregelmäßig
-                idxCrack = np.arange(0,8,1) # Index für die Crack-Prüfungen
-                idxFlow = np.arange(0,1,1) #Index für die Flow-Prüfungen
                 SN = data['S/N']
+
+                #entferne die Kopfzeilen
                 header = SN != 'S/N'
                 data = data[header]
+
+                #lese meta-daten aus
                 date = data['S/N'][0]
                 origin = data['pos'][0]
                 operator = data['Crack_Pressure_Cycle1'][0]
                 order = data['Crack_Pressure_Cycle2'][0]
                 delivery = data['Crack_Pressure_Cycle3'][0]
                 delPos = data['Crack_Pressure_Cycle4'][0]
+
+                #entferne die Zeilen mit Metadaten
                 R1 = data['Reseat_Pressure_Cycle1']
                 header = ~R1.isna()
-
                 data = data[header]
+
+                #Konvertiere die Datentypen string -> double/int in numpy - Array
                 Flow = data['Flow'].to_numpy(dtype='double')
                 FP = data['Flow_Pressure'].to_numpy(dtype='double')
                 SN = data['S/N'].to_numpy(dtype='int')
@@ -58,16 +71,25 @@ for root, dirs, files in os.walk(USB):
                 R6 = data['Reseat_Pressure_Cycle6'].to_numpy(dtype='double')
                 R7 = data['Reseat_Pressure_Cycle7'].to_numpy(dtype='double')
 
+                #Erstelle Datenframe aus numpy-Spalten
                 testData = pd.DataFrame()
-                testData['SN'], testData['Flow'], testData['FP'] = SN, Flow, FP,
-                testData['C1'], testData['R1'] = C1, R1
-                testData['C2'], testData['R2'] = C2, R2
-                testData['C3'], testData['R3'] = C3, R3
-                testData['C4'], testData['R4'] = C4, R4
-                testData['C5'], testData['R5'] = C5, R5
-                testData['C6'], testData['R6'] = C6, R6
-                testData['C7'], testData['R7'] = C7, R7
+                testData['SN'], testData['Flow'], testData['FP'] = SN, Flow, FP
+                testData['C1'] = C1
+                testData['C2'] = C2
+                testData['C3'] = C3
+                testData['C4'] = C4
+                testData['C5'] = C5
+                testData['C6'] = C6
+                testData['C7'] = C7
+                testData['R1'] = R1
+                testData['R2'] = R2
+                testData['R3'] = R3
+                testData['R4'] = R4
+                testData['R5'] = R5
+                testData['R6'] = R6
+                testData['R7'] = R7
 
+                #Berechne die Felder in denen Der Reseat-Pressure größer oder gleich dem Crack-Pressure ist
                 val = pd.DataFrame()
                 val["Cycle0"] = testData['R1'] >= testData['C1']
                 val["Cycle1"] = testData['R2'] >= testData['C2']
@@ -76,12 +98,15 @@ for root, dirs, files in os.walk(USB):
                 val["Cycle4"] = testData['R5'] >= testData['C5']
                 val["Cycle5"] = testData['R6'] >= testData['C6']
                 val["Cycle6"] = testData['R7'] >= testData['C7']
+
+                #Berechne die Felder in denen der Flow kleiner FLOW und Druck größer DRUCK ist.
                 valFlow = pd.DataFrame()
-                valFlow = testData['Flow'] < 8
-                valFlowPressure = testData['FP'] > 3
+                valFlow = testData['Flow'] < FLOW
+                valFlowPressure = testData['FP'] > DRUCK
+
+                #Initialisiere Fehler-Array und schreibe die Fehler und Fehlerindizes in die Arrays
                 errors = []
                 errorIndex = []
-
                 if valFlowPressure.any():
                     for i, element in enumerate(valFlowPressure):
                         if element:
@@ -127,14 +152,14 @@ for root, dirs, files in os.walk(USB):
                         if element:
                             errors.append("Fehler bei Ventil No.: "+str(SN[i]) +" in Zyklus 7")
                             errorIndex.append(['Cycle6', i])
-                print(errorIndex)
-                FILE = "Ergebnis5.md"
+                #print(errorIndex)
+                FILE = ERGEBNIS
                 
                 with open (FILE, 'a')as file:
                     file.write('\n______\n')
                     file.write(f" ## Messergebnise aus {origin}\n\n")
                     file.write('\n')
-                    file.write(f'Aus Datei: {origin} Prüfer: {operator} Datum: {date} Auftrag: {order} Lieferschein: {delivery} Lieferscheinposition: {delPos}' )
+                    file.write(f"Aus Datei: {origin} Prüfer: {operator} Datum: {date} Auftrag: {order} Lieferschein: {delivery} Lieferscheinposition: {delPos}")
                     file.write('\n\n')
                     file.close()
                 testData.to_markdown(FILE, mode = 'a')
