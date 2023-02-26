d847bcc7c11f9745848bcc580f0dff5decffc547
LS-KS <116895972+LS-KS@users.noreply.github.com>
2023-02-26 19:32:31 +0100

    Canvas-Window at the end of the script

--- Service/TkService.py
+++ Service/TkService.py
@@ -1,5 +1,11 @@
+import tkinter
 from tkinter import messagebox
 import tkinter as Tk
+
+import numpy as np
+import pandas as pd
+
+
 def showNotification(errors):
     if len(errors) == 0:
 
@@ -10,5 +16,17 @@ def showNotification(errors):
                                "Es wurden Fehler in den Messungen gefunden. Öffne die Datei result.md um anzuzeigen")
 
 
-def showResultsInGrid(testData, errors, index):
+def showResultsInGrid(testData, errors):
     root = Tk.Tk()
+    root.winfo_toplevel().title("Messdaten von Verzeichnis")
+    root.canvas = Tk.Canvas(root, width=500, height=500, borderwidth=1, highlightthickness=0)
+    root.canvas.pack(side="top", fill="both", expand="true")
+    numRows, numCols = testData.shape
+
+    for row in range(numRows):
+        for col in range(numCols):
+            cellValue = testData.iloc[row, col]
+            color = "black" if not errors[row, col] else "red"
+            root.canvas.create_text(col * 100 + 50, row * 50 + 25, text=str(cellValue), fill=color)
+
+    root.mainloop()
--- Service/errorService.py
+++ Service/errorService.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -20,47 +21,71 @@ def createErrors(val, valFlow, valFlowPressure, valCrackMax, valCrackMin, valRes
         if valCrackMax[cycle].any():
             for i, element in enumerate(valCrackMax[cycle]):
                 if element:
-                    errors.append("Fehler bei Ventil No.: " + str(SN[i]) + "Crack-Limit überschritten"+ "in Zyklus" + str(cycles.index(cycle) + 1))
+                    errors.append(
+                        "Fehler bei Ventil No.: " + str(SN[i]) + "Crack-Limit überschritten" + "in Zyklus" + str(
+                            cycles.index(cycle) + 1))
                     errorIndex.append(['crack-Maximum', i])
         if valCrackMin[cycle].any():
             for i, element in enumerate(valCrackMax[cycle]):
                 if element:
-                    errors.append("Fehler bei Ventil No.: " + str(SN[i]) + "Crack-Limit unterschritten"+ "in Zyklus" + str(cycles.index(cycle) + 1))
+                    errors.append(
+                        "Fehler bei Ventil No.: " + str(SN[i]) + "Crack-Limit unterschritten" + "in Zyklus" + str(
+                            cycles.index(cycle) + 1))
                     errorIndex.append(['Crack-Minimum', i])
         if valReseatMin[cycle].any():
             for i, element in enumerate(valReseatMin[cycle]):
                 if element:
-                    errors.append("Fehler bei Ventil No.: " + str(SN[i]) + "Reseat-Limit unterschritten"+ "in Zyklus" + str(cycles.index(cycle) + 1))
+                    errors.append(
+                        "Fehler bei Ventil No.: " + str(SN[i]) + "Reseat-Limit unterschritten" + "in Zyklus" + str(
+                            cycles.index(cycle) + 1))
                     errorIndex.append(['Reseat-Minimum', i])
         if val[cycle].any():
             for i, element in enumerate(val[cycle]):
                 if element:
-                    errors.append("Fehler bei Ventil No.: " + str(SN[i]) + " in Zyklus" + str(cycles.index(cycle) + 1)+": Reseat-Pressure > Crack-Pressure")
+                    errors.append("Fehler bei Ventil No.: " + str(SN[i]) + " in Zyklus" + str(
+                        cycles.index(cycle) + 1) + ": Reseat-Pressure > Crack-Pressure")
                     errorIndex.append([cycle, i])
 
     return errors, errorIndex
 
-def createErrorsBySN(val, valFlow, valFlowPressure, valCrackMax, valCrackMin, valReseatMin, SN):
+
+def setReds(value, errors, x, y):
+    errors[x][y] = value
+    if value:
+        errors[0][y] = True
+    return errors
+def createErrorsBySN(val: pd.DataFrame, valFlow: pd.DataFrame, valFlowPressure: pd.DataFrame, valCrackMax: pd.DataFrame,
+                     valCrackMin: pd.DataFrame, valReseatMin: pd.DataFrame, SN: pd.DataFrame):
     errors = []
+    reds = np.zeros((valCrackMax.shape[1] + 3 + valReseatMin.shape[1], valCrackMax.shape[0]), dtype=bool)
     cycles = ["Cycle0", "Cycle1", "Cycle2", "Cycle3", "Cycle4", "Cycle5", "Cycle6"]
     for i, element in enumerate(SN):
         if valFlow[i]:
             errors.append("Fehler bei Ventil No.: " + str(element) + " im Flow-Druck")
+            reds = setReds(True, reds, 1, i)
         if valFlowPressure[i]:
             errors.append("Fehler bei Ventil No.: " + str(element) + " im Flow-Druck")
+            reds = setReds(True, reds, 2, i)
         for j, cycle in enumerate(cycles):
             if valCrackMax[cycle][i]:
-                errors.append("Fehler bei Ventil No.: " + str(element) + "Crack-Limit überschritten" + "in Zyklus" + str(
-                    j + 1))
+                errors.append(
+                    "Fehler bei Ventil No.: " + str(element) + "Crack-Limit überschritten" + "in Zyklus" + str(
+                        j + 1))
+                reds = setReds(True, reds, cycles.index(cycle)+3, i)
             if valCrackMin[cycle][i]:
                 errors.append(
                     "Fehler bei Ventil No.: " + str(element) + "Crack-Limit unterschritten" + "in Zyklus" + str(
                         j + 1))
+                reds = setReds(True, reds, cycles.index(cycle)+3, i)
             if valReseatMin[cycle][i]:
                 errors.append(
                     "Fehler bei Ventil No.: " + str(element) + "Reseat-Limit unterschritten" + "in Zyklus" + str(
                         j + 1))
+                reds = setReds(True, reds, cycles.index(cycle)+3 + valCrackMax.shape[1], i)
             if val[cycle][i]:
                 errors.append("Fehler bei Ventil No.: " + str(element) + " in Zyklus" + str(
                     j + 1) + ": Reseat-Pressure > Crack-Pressure")
-    return errors
+                reds = setReds(True, reds, cycles.index(cycle)+3 + valCrackMax.shape[1], i)
+                reds = setReds(True, reds, cycles.index(cycle) + 3, i)
+
+    return errors, reds
