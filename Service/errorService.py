5c9a79cec16428c6a2de9c6afb906399dafe8cd1
LS-KS <116895972+LS-KS@users.noreply.github.com>
2023-02-27 08:25:29 +0100

    Fehler im Fehlertext vom Durchfluss behhoben

--- Service/errorService.py
+++ Service/errorService.py
@@ -2,53 +2,6 @@ import numpy as np
 import pandas as pd
 
 
-def createErrors(val, valFlow, valFlowPressure, valCrackMax, valCrackMin, valReseatMin, SN):
-    # Initialisiere Fehler-Array und schreibe die Fehler und Fehlerindizes in die Arrays
-    errors = []
-    errorIndex = []
-    cycles = ["Cycle0", "Cycle1", "Cycle2", "Cycle3", "Cycle4", "Cycle5", "Cycle6"]
-    if valFlowPressure.any():
-        for i, element in enumerate(valFlowPressure):
-            if element:
-                errors.append("Fehler bei Ventil No.: " + str(SN[i]) + " im Flow-Druck")
-                errorIndex.append(['FP', i])
-    if valFlow.any():
-        for i, element in enumerate(valFlow):
-            if element:
-                errors.append("Fehler bei Ventil No.: " + str(SN[i]) + " im Flow")
-                errorIndex.append(['Flow', i])
-    for cycle in cycles:
-        if valCrackMax[cycle].any():
-            for i, element in enumerate(valCrackMax[cycle]):
-                if element:
-                    errors.append(
-                        "Fehler bei Ventil No.: " + str(SN[i]) + "Crack-Limit überschritten" + "in Zyklus" + str(
-                            cycles.index(cycle) + 1))
-                    errorIndex.append(['crack-Maximum', i])
-        if valCrackMin[cycle].any():
-            for i, element in enumerate(valCrackMax[cycle]):
-                if element:
-                    errors.append(
-                        "Fehler bei Ventil No.: " + str(SN[i]) + "Crack-Limit unterschritten" + "in Zyklus" + str(
-                            cycles.index(cycle) + 1))
-                    errorIndex.append(['Crack-Minimum', i])
-        if valReseatMin[cycle].any():
-            for i, element in enumerate(valReseatMin[cycle]):
-                if element:
-                    errors.append(
-                        "Fehler bei Ventil No.: " + str(SN[i]) + "Reseat-Limit unterschritten" + "in Zyklus" + str(
-                            cycles.index(cycle) + 1))
-                    errorIndex.append(['Reseat-Minimum', i])
-        if val[cycle].any():
-            for i, element in enumerate(val[cycle]):
-                if element:
-                    errors.append("Fehler bei Ventil No.: " + str(SN[i]) + " in Zyklus" + str(
-                        cycles.index(cycle) + 1) + ": Reseat-Pressure > Crack-Pressure")
-                    errorIndex.append([cycle, i])
-
-    return errors, errorIndex
-
-
 def setReds(value, reds, row, col):
     reds[row][col] = value
     if value:
@@ -61,10 +14,10 @@ def createErrorsBySN(val: pd.DataFrame, valFlow: pd.DataFrame, valFlowPressure: 
     cycles = ["Cycle0", "Cycle1", "Cycle2", "Cycle3", "Cycle4", "Cycle5", "Cycle6"]
     for i, element in enumerate(SN):
         if valFlow[i]:
-            errors.append("Fehler bei Ventil No.: " + str(element) + " im Flow-Druck")
+            errors.append("Fehler bei Ventil No.: " + str(element) + " im Flow")
             reds = setReds(True, reds, i, 1)
         if valFlowPressure[i]:
-            errors.append("Fehler bei Ventil No.: " + str(element) + " im Flow")
+            errors.append("Fehler bei Ventil No.: " + str(element) + " im Flow-Druck")
             reds = setReds(True, reds, i, 2)
         for j, cycle in enumerate(cycles):
             if valCrackMax[cycle][i]:
