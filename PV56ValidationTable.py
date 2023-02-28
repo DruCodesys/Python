b00e6898ef52b7fa52295fa5aebb976b8b6a249c
LS-KS <116895972+LS-KS@users.noreply.github.com>
2023-02-28 09:20:17 +0100

    feature: handleBadLine in main-Script

--- PV56ValidationTable.py
+++ PV56ValidationTable.py
@@ -13,10 +13,10 @@ from Service import EvalService, DataService, errorService, TkService
 
 # ------PROGRAMMPARAMETER-----#
 # TODO: Die Variable 'USB' muss das Hauptverzeichnis des USB-Sticks sein.
-#USB = "C:/Users/pink_/Documents/GitHub/Python/"
+USB = "C:/Users/pink_/Documents/GitHub/Python/"
 #USB = "C:/Users/lschi/Documents/GitHub/Python/"
 #USB = "C:\\Users\\lschi\\Desktop\\Files"
-USB = "D:\\"
+#USB = "D:\\"
 # TODO: Bemessungsgrenze für Durchflusswert und Druck
 DRUCK = 3
 FLOW = 8
@@ -30,6 +30,15 @@ RESEATMIN = 1.6
 # TODO: Bitte Beachten: DIe Ergebnisdatei manuell löschen! Dieses Script hängt immer nur an die aktuelle Datei an.
 ERGEBNIS = "Ergebnis.md"
 
+#Function to handle bad line import:
+def handleBadLine(badLine):
+    # Remove last character (which is a tabulator) from bad lines
+    if badLine[-1] == '\t':
+        badLine = badLine[:-1]
+    # Return the modified list of strings
+    return badLine
+
+
 # -----PROGRAMM-----#
 # For-Schleife wird für jede Datei im Verzeichnis 'USB' ausgeführt
 for root, dirs, files in os.walk(USB): #os.walk is a Generator. Not possible to store in variables once
@@ -37,7 +46,7 @@ for root, dirs, files in os.walk(USB): #os.walk is a Generator. Not possible to 
     for FILE in files:
         if FILE.endswith(".csv"):
             # FILE ist tabulator getrennte csv-Datei
-            data = pd.read_csv(f"{USB}{FILE}", delimiter="\t", error_bad_lines=False)
+            data = pd.read_csv(f"{USB}{FILE}", delimiter="\t", on_bad_lines=handleBadLine, engine='python')
 
             testData, SN, metaData = DataService.DataService(data)
             # Berechne boolsche Matritzen für Auswertung
